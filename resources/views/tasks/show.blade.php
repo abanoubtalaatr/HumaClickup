6ab14ca26bd05055f014a404e267f0d0418c00ed
abanoub-talaat <105149048+abanoub-talaat@users.noreply.github.com>
2026-01-26 20:22:19 +0200

    wip

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -16,6 +16,8 @@
                 </p>
             </div>
             <div class="flex items-center space-x-3">
+                
+                @if(Auth::id() === $task->user_id)
                 <a href="{{ route('tasks.edit', $task) }}" 
                    class="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50">
                     Edit
@@ -28,6 +30,7 @@
                         Delete
                     </button>
                 </form>
+                @endif
             </div>
             
         </div>
