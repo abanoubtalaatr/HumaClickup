a039113d6b3ed330ee487c09d8c6ec02758738e9
abanoub-talaat <105149048+abanoub-talaat@users.noreply.github.com>
2026-01-27 18:18:53 +0200

    wip

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -17,7 +17,7 @@
             </div>
             <div class="flex items-center space-x-3">
                 
-                @if(Auth::id() === $task->user_id)
+                @if(Auth::id() === $task->creator_id)
                 <a href="{{ route('tasks.edit', $task) }}" 
                    class="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50">
                     Edit
